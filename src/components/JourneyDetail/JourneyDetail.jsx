f52e1715d3f7d762b271fa9d4171170229820167
Mikko Kaario <mikko.kaario@gmail.com>
2023-01-29 18:18:32 +0200

    JourneyDetail element for the JourneyView + some basic styles

---
+++ src/components/JourneyDetail/JourneyDetail.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import classes from './JourneyDetail.module.css'
+
+const JourneyDetail = (props) => {
+  return (
+    <div className={classes.journey_detail}>
+        
+        <h3>
+            J<span className={classes.header_number}>{props.id}</span>
+        </h3>
+        <div className={classes.detail_element}>{props.distance}</div>
+
+        <div className={classes.detail_element}>{props.duration}</div>
+        
+        <div className={classes.detail_element}>{props.departureStationNumber}, {props.departureStationName}</div>
+
+        <div className={classes.detail_element}>{props.returnStationNumber}, {props.returnStationName}</div>
+
+    </div>
+  )
+}
+
+
+export default JourneyDetail
