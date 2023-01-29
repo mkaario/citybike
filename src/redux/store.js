8bc5a03e1eb419fb47ba43ee70391ce2a9c9ca4d
Mikko Kaario <mikko.kaario@gmail.com>
2023-01-29 17:36:53 +0200

    imported journeys and stations reducers to the store and exported actions forward

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import journeysReducer from './slices/journeysSlice';
+import stationsReducer from './slices/stationsSlice';
 
 const store = configureStore({
     reducer:{
-
+        storedJourneys: journeysReducer,
+        storedStations: stationsReducer
     }
 })
+
+export const journeysActions = journeysReducer.actions;
+export const stationsActions = stationsReducer.actions;
 
 export default store
