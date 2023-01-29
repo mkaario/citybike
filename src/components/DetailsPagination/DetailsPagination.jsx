36a0eacc56f5ead8d54e3ee8fec922da8c838d58
Mikko Kaario <mikko.kaario@gmail.com>
2023-01-29 18:10:19 +0200

    Preparing the Pagination element

---
+++ src/components/DetailsPagination/DetailsPagination.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Pagination } from '@mui/material';
+import classes from './DetailsPagination.module.css'
+
+const DetailsPagination = ({itemsPerPage, totalItems, onChange}) => {
+const count = Math.ceil(totalItems / itemsPerPage);
+
+  return (
+    <Pagination className={classes.pagination_block} onChange={onChange} count={count}/>
+  )
+}
+
+export default DetailsPagination
