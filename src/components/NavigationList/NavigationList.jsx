77d52fbedf9c9fded5a00d8d6eb35c95926276be
Mikko Kaario <mikko.kaario@gmail.com>
2023-01-30 15:15:27 +0200

    Icons for navigation + styles for active states

--- src/components/NavigationList/NavigationList.jsx
+++ src/components/NavigationList/NavigationList.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import classes from './NavigationList.module.scss'
+import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
 
 const NavigationList = () => {
-  return (
+    return (
     <div className={classes.navigation_wrapper}>
-        <NavLink className={({isActive})=> isActive ? classes.active : undefined} to='/journeys'>Journeys</NavLink>
-        <NavLink className={({isActive})=> isActive ? classes.active : undefined} to='/stations'>Stations</NavLink>
+        <NavLink className={({isActive}) => isActive ? classes.active : undefined} to='/journeys'><DirectionsBikeIcon/>Journeys</NavLink>
+        <NavLink className={({isActive}) => isActive ? classes.active : undefined} to='/stations'>Stations<LocationOnIcon/></NavLink>
     </div>
   )
 }
