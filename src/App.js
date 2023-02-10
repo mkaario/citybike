3cf59eb8ba6a43c8d152267c5db4a0d47a05a2ca
mkaario <mikko.kaario@gmail.com>
2023-02-10 12:44:41 +0200

    Populate journeys & stations only once on app load

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import './App.scss';
-import RootLayout from './pages/RootLayout';
-import ErrorPage from './pages/ErrorPage';
-import HomePage from './pages/HomePage';
-import JourneysPage from './pages/JourneysPage';
-import StationsPage from './pages/StationsPage';
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import { getJourneysData } from './redux/data-actions/journey-actions'
-import { getStationsData } from './redux/data-actions/station-actions'
+import "./App.scss";
+import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
+import JourneysPage from "./pages/JourneysPage";
+import StationsPage from "./pages/StationsPage";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { getJourneysData } from "./redux/data-actions/journey-actions";
+import { getStationsData } from "./redux/data-actions/station-actions";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <RootLayout/>,
-    errorElement: <ErrorPage/>,
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
-      {path: '/', element: <HomePage/>},
-      {path: '/journeys', element: <JourneysPage/>},
-      {path: '/stations', element: <StationsPage/>}
-    ]
-  }
-])
+      { path: "/", element: <HomePage /> },
+      { path: "/journeys", element: <JourneysPage /> },
+      { path: "/stations", element: <StationsPage /> },
+    ],
+  },
+]);
+
+let didInit = false;
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getJourneysData());
-    dispatch(getStationsData());
-  },[dispatch])
+    if (!didInit) {
+      didInit = true;
+      dispatch(getJourneysData());
+      dispatch(getStationsData());
+    }
+  }, [dispatch]);
 
-
-  return (
-    <RouterProvider router={router}/>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
